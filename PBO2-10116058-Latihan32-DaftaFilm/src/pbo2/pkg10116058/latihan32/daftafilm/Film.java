590a6b9e85c805332e527946776f4c13d0b706be
nialditanawara <31877365+nialditanawara@users.noreply.github.com>
2019-02-19 01:49:16 +0700

    Add files via upload

---
+++ PBO2-10116058-Latihan32-DaftaFilm/src/pbo2/pkg10116058/latihan32/daftafilm/Film.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10116058.latihan32.daftafilm;
+
+public class Film {
+
+    public String filmName;
+    public String filmGenre;
+    public Double filmRating;
+    public int filmDuration;
+
+    public void nowPlaying(String filmName, String filmGenre, Double filmRating,
+            int filmDuration) {
+        System.out.println("Judul Film\t:" + filmName);
+        System.out.println("Genre Film\t:" + filmGenre);
+        System.out.println("Rating Film\t:" + filmRating);
+        System.out.println("Duration Film\t:" + filmDuration + " Menit");
+    }
+
+}
